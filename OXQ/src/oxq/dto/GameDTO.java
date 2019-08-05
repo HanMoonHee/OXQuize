2acc08b689af9e46469bde8d4c51b6da200583f4
yongje93 <48149636+yongje93@users.noreply.github.com>
2019-08-05 18:50:10 +0900

    DTO update

--- OXQ/src/oxq/dto/GameDTO.java
+++ OXQ/src/oxq/dto/GameDTO.java
@@ -5,11 +5,13 @@ public class GameDTO {
 	private int wincount;		// 1등한 횟수
 	private int o_count;		// 맞춘 개수
 	private int x_count;		// 틀린 개수
-	// getter
+	
+	// getters
 	public String getMember_id() {return member_id;}
 	public int getWincount() {return wincount;}
 	public int getO_count() {return o_count;}	
 	public int getX_count() {return x_count;}
+	
 	// setter
 	public void setMember_id(String member_id) {this.member_id = member_id;}
 	public void setWincount(int wincount) {this.wincount = wincount;}
