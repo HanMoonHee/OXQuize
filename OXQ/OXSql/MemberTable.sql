6132827fc7140aabf88f269b4f242cd649b03cdd
kimtahun13 <kimtahun11@naver.com>
2019-08-07 14:53:09 +0900

    SQL 수정
    on_off 추가

---
+++ OXQ/OXSql/MemberTable.sql
@@ -0,0 +1,10 @@
+create table member(
+id number(20) primary key,
+pwd varchar2(100),
+nickname varchar2(100),
+tel varchar2(20),
+email varchar2(100)
+);
+
+select * from member;
+drop table members;
